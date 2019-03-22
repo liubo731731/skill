1f9a00c55a8ffdb03097a51d94b58ad305da4ca9
liubo731731 <1083428842@qq.com>
2019-03-22 14:36:10 +0800

    修改之后的第一版模板

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,93 +1,87 @@
-var myApp = angular.module("myApp", ['ui.router']);
+(function () {
+    'use strict';
+    angular
+        .module('php', [
+            'app.sidebar',
+			'app.routes'
+        ]);
+})();
+(function () {
+    'use strict';
+    angular
+        .module('app.sidebar', []);
+})();
+(function () {
+    'use strict';
+    angular
+        .module('app.routes', ['ui.router']);
+})();
 
-myApp.config(function ($stateProvider, $urlRouterProvider) {
+(function () {
+    'use strict';
+    angular
+        .module('app.sidebar')
+        .controller('SidebarController', SidebarController);
 
-     $urlRouterProvider.when("", "/PageTab");
+    SidebarController.$inject = ['$rootScope', '$scope'];
+    function SidebarController($rootScope, $scope) {
+	autoLeftNav();
+	$(window).resize(function() {
+	    autoLeftNav();
+	    console.log($(window).width())
+	});
+	function autoLeftNav() {
+		    $('.tpl-header-switch-button').on('click', function() {
+		        if ($('.left-sidebar').is('.active')) {
+		            if ($(window).width() > 1024) {
+		                $('.tpl-content-wrapper').removeClass('active');
+		            }
+		            $('.left-sidebar').removeClass('active');
+		        } else {
+		
+		            $('.left-sidebar').addClass('active');
+		            if ($(window).width() > 1024) {
+		                $('.tpl-content-wrapper').addClass('active');
+		            }
+		        }
+		    })
+		
+		    if ($(window).width() < 1024) {
+		        $('.left-sidebar').addClass('active');
+		    } else {
+		        $('.left-sidebar').removeClass('active');
+		    }
+		}
+		
+		
+		// 侧边菜单
+ 		$('.sidebar-nav-sub-title').on('click', function() {
+ 		    $(this).siblings('.sidebar-nav-sub').slideToggle(80)
+ 		        .end()
+ 		        .find('.sidebar-nav-sub-ico').toggleClass('sidebar-nav-sub-ico-rotate');
+		})	
+		
+	}
 
-     $stateProvider
-        .state("PageTab", {
-            url: "/PageTab",
-            templateUrl: "form.html"
-        })
-        .state("PageTab.Page1", {
-            url:"/Page1",
-            templateUrl: "Page-1.html"
-        })
-        .state("PageTab.Page2", {
-            url:"/Page2",
-            templateUrl: "Page-2.html"
-        })
-        .state("PageTab.Page3", {
-            url:"/Page3",
-            templateUrl: "Page3.html"
-        });
-});
-
-
-
-
-$(function() {
-    autoLeftNav();
-    $(window).resize(function() {
-        autoLeftNav();
-        console.log($(window).width())
-    });
-})
-
-
-// 页面数据
-var pageData = {}
-
-
-// 风格切换
-
-$('.tpl-skiner-toggle').on('click', function() {
-    $('.tpl-skiner').toggleClass('active');
-})
-
-$('.tpl-skiner-content-bar').find('span').on('click', function() {
-    $('body').attr('class', $(this).attr('data-color'))
-    saveSelectColor.Color = $(this).attr('data-color');
-    // 保存选择项
-    storageSave(saveSelectColor);
-})
-
-
-
-
-// 侧边菜单开关
-
-
-function autoLeftNav() {
-
-
-
-    $('.tpl-header-switch-button').on('click', function() {
-        if ($('.left-sidebar').is('.active')) {
-            if ($(window).width() > 1024) {
-                $('.tpl-content-wrapper').removeClass('active');
-            }
-            $('.left-sidebar').removeClass('active');
-        } else {
-
-            $('.left-sidebar').addClass('active');
-            if ($(window).width() > 1024) {
-                $('.tpl-content-wrapper').addClass('active');
-            }
-        }
-    })
-
-    if ($(window).width() < 1024) {
-        $('.left-sidebar').addClass('active');
-    } else {
-        $('.left-sidebar').removeClass('active');
-    }
-}
+})();
+(function($) {
+	  angular.module('app.routes').config(routeConfig);
+		routeConfig.$inject = ['$stateProvider', '$urlRouterProvider','$locationProvider'];
+		function  routeConfig($stateProvider, $urlRouterProvider,$locationProvider) {
+        $locationProvider.hashPrefix('');
+		      $locationProvider.html5Mode(false);
+	          $urlRouterProvider.otherwise('/form');
+		      $stateProvider
+				.state('form', { //单一视图
+					url: '/form',
+					templateUrl: 'form.html',
+			   
+			   })
+			   .state('404', { //单一视图
+					url: '/404',
+					templateUrl: '404.html',
+				})     
+	};
+})()
 
 
-// 侧边菜单
-$('.sidebar-nav-sub-title').on('click', function() {
-    $(this).siblings('.sidebar-nav-sub').slideToggle(80)
-        .end()
-        .find('.sidebar-nav-sub-ico').toggleClass('sidebar-nav-sub-ico-rotate');
-})

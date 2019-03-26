c915b640a20ba25755597131647819e092f8dc61
liubo731731 <1083428842@qq.com>
2019-03-26 10:22:59 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -94,7 +94,7 @@
 					controller: function($scope,$stateParams){
 						
 						   jQuery.ajax({
-						    url: './md/'+$stateParams.mdName.replace(/./g, '/')+'.md?t='+(new Date().getTime()/60000),
+						    url: './md/'+$stateParams.mdName.replace(/\./g, '/')+'.md?t='+(new Date().getTime()/60000),
 						    type: "GET",
 						    dataType: "text",
 						    success: function (data) {
